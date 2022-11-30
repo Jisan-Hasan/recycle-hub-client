d2a66f2b89ea99b5bb6111b7b2a8738e5715c96c
Jisan-Hasan <jisan.jh5@gmail.com>
2022-11-30 17:36:13 +0600

    show all categories on ui

---
+++ src/Pages/Home/Categories/Categories.js
@@ -0,0 +1,34 @@
+import { data } from "autoprefixer";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+const Categories = () => {
+    const [categories, setCategories] = useState([]);
+    useEffect(() => {
+        fetch(`${process.env.REACT_APP_API_URL}/categories`)
+            .then((res) => res.json())
+            .then((data) => {
+                setCategories(data);
+            });
+    }, []);
+    return (
+        <div>
+            <h2 className="mt-24 my-14 text-3xl font-bold text-center">
+                All Categories
+            </h2>
+            <div className="grid grid-cols-2 md:grid-cols-4 gap-10">
+                {categories.map((category) => (
+                    <Link
+                        key={category._id}
+                        to={`/category/${category._id}`}
+                        className="btn btn-info py-24 font-bold text-3xl text-center"
+                    >
+                        {category.name}
+                    </Link>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Categories;
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "../Banner/Banner";
+import Categories from "../Categories/Categories";
 
 const Home = () => {
     return (
         <div className="container mx-auto my-5 md:my-28">
             <Banner />
+            <Categories />
         </div>
     );
 };
