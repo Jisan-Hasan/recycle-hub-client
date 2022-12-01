20af961408bbc58953c5179ac78be051dc09c64c
Jisan-Hasan <jisan.jh5@gmail.com>
2022-12-02 05:09:53 +0600

    advertised items showed on UI

--- src/Pages/Home/AdvertisedItems/AdvertisedItem.js
+++ src/Pages/Home/AdvertisedItems/AdvertisedItem.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import Product from "../../Products/Product";
 
 const AdvertisedItem = () => {
+    const [items, setItems] = useState([]);
+    useEffect(() => {
+        fetch(`${process.env.REACT_APP_API_URL}/advertisedProduct`)
+            .then((res) => res.json())
+            .then((data) => {
+                setItems(data);
+            });
+    }, []);
+
+
+    if (items.length < 1) {
+        return;
+    }
     return (
         <div>
-            Advertised Item
+            <h2 className="text-2xl font-bold text-center mt-24 mb-8">Advertised Items</h2>
+            <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+                {
+                    items.map(item => <Product key={item._id} product={item} />)
+                }
+            </div>
         </div>
     );
 };
